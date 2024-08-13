323b30949e766f54baeea34808e8c9f2e1ab5bff
RAIYAN-AHMED496 <meherabhossainraiyan@gmail.com>
2024-08-13 22:41:22 +0600

    First ptyhon file adding

---
+++ main.py
@@ -0,0 +1,3 @@
+# I am creating a new python file for git practice 
+# it is good to see that git is a stable software
+## IF WE WANT TO SEE WHAT THE SPECIFIC DIRECTORY WE ARE WORKING THEN WE CAN WRITE pwd means present working directory
