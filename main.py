5abc56582caa9a76e36bc27f2638f6abaf0d320e
RAIYAN-AHMED496 <meherabhossainraiyan@gmail.com>
2024-08-13 22:43:09 +0600

    adding new lines

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 # I am creating a new python file for git practice 
 # it is good to see that git is a stable software
 ## IF WE WANT TO SEE WHAT THE SPECIFIC DIRECTORY WE ARE WORKING THEN WE CAN WRITE pwd means present working directory
+'''  
+WHEN YOU START TO LEARN GIT YOU SHOULD PRACTICE HOW TO CHANGE DIRECTORY AND THEN HOW TO ADD AND HOW TO COMMIT
+1) TO CHANGE DIRECTORY-----> cd <name of the directory>
+2) HOW TO ADD------> git add .
+3) HOW TO COMMIT------> git commit -m "give a relevent comment(not necessary but useful)
+
+
+
+
+'''
